0d1c4654d36d43b289116c118967cacf1f875b34
abaduna~ <artuelrey@gmail.com>
2024-01-11 16:06:13 -0300

    backen y variables de entorno

--- serve/src/index.js
+++ serve/src/index.js
@@ -164,15 +164,14 @@ app.post("/api/users", async (req, res) => {
     res.status(500).json({ error: "Error de la consulta" });
   }
 });
+require("dotenv").config();
 mercadopago.configure({
-  access_token:
-    "",
+  access_token: process.env.ACCESS_TOKEN,
 });
 
 app.post("/mercadopago", async (req, res) => {
   const { data } = req.body;
   const { total, caritoDeCompras, nombre } = data;
-  console.log(req.body);
   try {
     const preference = {
       items: [
@@ -186,14 +185,20 @@ app.post("/mercadopago", async (req, res) => {
       ],
 
       back_urls: {
-        success: "http://localhost:3000/success/",
+        success: "http://localhost:3000/felicitaciones",
         failure: "http://localhost:3000/fallo",
       },
       auto_return: "approved",
     };
-    llemaralaapiComprarProducto(caritoDeCompras, nombre);
+    try {
+      console.log(`dataBody ${(caritoDeCompras, nombre, total)}`);
+      await llemaralaapiComprarProducto(caritoDeCompras, nombre, total); // vaa tener que ser un useEffec()
+    } catch (error) {
+      console.log(`error en llemaralaapiComprarProducto ${error}`);
+    }
+
     const respons = await mercadopago.preferences.create(preference);
-    console.log(respons);
+    console.log(respons.status); //201
 
     res.status(200).json(respons.response.init_point);
   } catch (error) {
@@ -203,28 +208,76 @@ app.post("/mercadopago", async (req, res) => {
 });
 const fetch = require("node-fetch");
 
-const llemaralaapiComprarProducto = async (caritoDeCompras, nombre) => {
-  const apiUrl = "http://localhost:3001/producBought";
+const llemaralaapiComprarProducto = async (caritoDeCompras, nombre, total) => {
   const dataBody = {
     caritoDeCompras,
-    nombre,
+    nombre: nombre,
+    total,
   };
+  console.log(`dataBody`);
+  console.log(dataBody);
   const response = await fetch("http://localhost:3001/producBough", {
     method: "POST",
-    body: dataBody,
+    body: JSON.stringify(dataBody),
+    headers: {
+      "Content-Type": "application/json", // Specify content type
+    },
   });
   const data = await response.json();
+  console.log(`data`);
+  console.log(data);
 };
-app.post("/producBought", (req, res) => {
+var uniqid = require("uniqid");
+
+app.post("/producBough", async (req, res) => {
+  const id_orden = await uniqid();
+  console.log(id_orden);
+  const { nombre, caritoDeCompras, total } = req.body;
+  console.log(`api /producBough`);
   console.log(req.body);
+
+  console.log(`caritoDeCompras`);
+  console.log(caritoDeCompras);
+  addTablaPrincipalProductosComprados(id_orden, nombre, total);
+  addTablaDeProductos(id_orden, caritoDeCompras);
+
   res.status(200).send("Agregado con exito la compra");
 });
+const addTablaPrincipalProductosComprados = async (id_orden, nombre, total) => {
+  let cuando_fue_comprado = Date.now(); //numero en millisegundos pasar a fecha desde 1 de enero 1970
+  console.log(cuando_fue_comprado);
+  const estado = "por entregar";
+  const connection = await database.getConnection();
+  console.log(nombre);
+  console.log(cuando_fue_comprado);
+  console.log(id_orden);
+  console.log(estado);
+  console.log(total);
+
+  try {
+    const consult =
+      "INSERT INTO `tablaprincipalproductoscomprados` (`nombre`, `cuando_fue_comprado`, `id_orden`,`estado`,`total`) VALUES (?,?,?,?,?);";
+    const variables = [nombre, cuando_fue_comprado, id_orden, estado, total];
+    await connection.query(consult, variables);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const addTablaDeProductos = async (id_orden, caritoDeCompras) => {
+  const connection = await database.getConnection();
+  console.log(`caritoDeCompras`);
+  console.log(caritoDeCompras);
+  // for (const producto of caritoDeCompras) {
+  //   await connection.execute('INSERT INTO tabladeproductos (id_orden, nombre, precio,descripcion) VALUES (?, ?, ?, ?)', [id_orden, producto.title, producto.precio, producto.descripction]);
+  // }}
+  for (let index = 0; index < caritoDeCompras.length; index++) {
+    const producto = caritoDeCompras[index];
+    await connection.query(
+      "INSERT INTO tabladeproductos (id_orden, nombre, precio,descripcion) VALUES (?, ?, ?, ?)",
+      [id_orden, producto.title, producto.precio, producto.descripction]
+    );
+  }
+};
 app.listen(3001, () => {
   console.log(`corriendo por el puerto 3001`);
 });
-
-/*
-este codigo necesita una refactorizacion urgente
-RowDataPacket { coreo: 'admin@gmail.com', password: '123456' },   
-RowDataPacket { coreo: 'artuelrey@gmail.com', password: '123456' }
-*/
