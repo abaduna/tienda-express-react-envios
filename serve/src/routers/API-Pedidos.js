332646e6734940d5d7cf8c6854fd6256b622d95e
abaduna~ <artuelrey@gmail.com>
2024-01-13 00:12:19 -0300

    CORRIGIDO UN ENDPONTIN

--- serve/src/routers/API-Pedidos.js
+++ serve/src/routers/API-Pedidos.js
@@ -2,8 +2,74 @@ const express = require(`express`);
 const database = require("./../db");
 
 const routerPedidos = express.Router();
-
+//obterner pedidos
 routerPedidos.get("/pedidos/entregados", async (req, res) => {
-  res.status(200).json({ message: "Delivered orders retrieved successfully" });
+  const connection = await database.getConnection();
+  try {
+    const result = await connection.query(
+      "SELECT * FROM tablaprincipalproductoscomprados;"
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error en la consulta:", error);
+    await connection.end();
+    res.status(500).json({ error: "Error en la consulta" });
+  }
 });
+//obtener productos
+routerPedidos.get("/pedidos/productList/:id", async (req, res) => {
+  const id_orden = req.params.id;
+  const connection = await database.getConnection();
+  try {
+    const result = await connection.query(
+      "SELECT * FROM tabladeproductos WHERE  id_orden = ?;",
+      [id_orden]
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error en la consulta:", error);
+    await connection.end();
+    res.status(500).json({ error: "Error en la consulta" });
+  }
+});
+
+routerPedidos.put("/pedidos/ModifyProductList/:id", async (req, res) => {
+  const id_orden = req.params.id;
+  const { nombre, precio, descripcion } = req.body;
+  try {
+    const SQLquery =
+      "INSERT INTO `tabladeproductos` (id_orden, nombre, precio,descripcion) VALUES (?, ?, ?.?);";
+    await connection.query(SQLquery, [id_orden, nombre, precio, descripcion]);
+
+    res.json({ mensaje: "Producto actualizado exitosamente" });
+  } catch (error) {
+    console.error("Error en la consulta:", error);
+    await connection.end();
+    res.status(500).json({ error: "Error en la consulta" });
+  }
+});
+routerPedidos.put("/pedidos/ModifyProduct/:id", async (req, res) => {
+  const id_orden = req.params.id;
+  const { nombre, total, descripcion } = req.body;
+  try {
+    const SQLquery =
+      "INSERT INTO `tablaprincipalproductoscomprados` (id_orden, nombre,cuando_fue_comprado ,estado,total) VALUES (?, ?, ?.?);";
+    await connection.query(SQLquery, [id_orden, total, precio, descripcion]);
+
+    await connection.end();
+    res.json({ mensaje: "Producto actualizado exitosamente" });
+  } catch (error) {
+    console.error("Error en la consulta:", error);
+    await connection.end();
+    res.status(500).json({ error: "Error en la consulta" });
+  }
+});
+//cambio de estado
+
+routerPedidos.patch("/pedidos/ModifyStatus/:id",(req,res)=>{
+  const SQLquery =
+  "INSERT INTO `tablaprincipalproductoscomprados` (estado) VALUES (?, ?, ?.?);";
+})
+
+
 module.exports = routerPedidos;
